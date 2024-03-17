13142fef56de229db61939a3be82cbda8f100d3a
Sebastiaan Eekhof <sebastiaan@eekhof.dev>
2024-03-17 14:19:36 +0100

    fix

--- ssr/EdgeLauncher.ts
+++ ssr/EdgeLauncher.ts
@@ -6,3 +6,8 @@ export async function SSRGetEncoderStatus(host: string) {
     const launcher = new EdgeLauncher(host);
     return await launcher.getEncoderStatus();
 }
+
+export async function SSRGetLavitaJobs(host: string) {
+    const launcher = new EdgeLauncher(host);
+    return await launcher.getLavitaJobs();
+}
